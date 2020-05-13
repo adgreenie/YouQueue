a0d8967cf5e01061a71c1e327dba38001018de94
Adam Green <adgreenie@gmail.com>
2020-05-13 15:50:39 -0400

    Date() wasn't giving current time on back end. Switched to Date.now, updated formatDate function on front end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { useState, createContext } from "react"
+import React, { useState, createContext, useEffect } from "react"
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Switcher from "./components/welcome/Switcher"
+import { getAllPosts } from "./services/api-helper"
+import { formatDate } from "./services/formatDate"
 import "./style.scss"
 
 export const AppContext = createContext()
 
 function App() {
   const [activeUsername, setActiveUsername] = useState(false)
+
+  useEffect(() => {
+    const makeAPICall = async () => {
+      // const resp = await getAllPosts()
+      // const date = new Date(resp[0].date)
+      // console.log("formatDate(date)", formatDate(date))
+    }
+    makeAPICall()
+  }, [])
 
   return (
     <AppContext.Provider value={{ activeUsername, setActiveUsername }}>
--- src/services/formatDate.js
+++ src/services/formatDate.js
@@ -1,16 +1,19 @@
-// returns an array in the format: ["Mon Apr 20 2020", "20:46"]
 export const formatDate = (rawDate) => {
-  const dateArr = rawDate.split("")
+  let hours = rawDate.getHours()
+  const minutes = rawDate.getMinutes()
+  const seconds = rawDate.getSeconds()
+  const zero = seconds < 10 ? "0" : ""
+  if (hours === 0 || hours === 12) {
+    hours += 12
+  }
+  const time =
+    hours < 12
+      ? `${hours}:${minutes}:${zero}${seconds} AM`
+      : `${hours - 12}:${minutes}:${zero}${seconds} PM`
 
-  const date = dateArr.slice(0, 15).join("")
+  const date = `${
+    rawDate.getMonth() + 1
+  }/${rawDate.getDate()}/${rawDate.getFullYear()}`
 
-  const hour = parseInt(dateArr.slice(16, 18).join(""))
-
-  const minute = dateArr.slice(19, 21).join("")
-
-  const time = hour > 12 ? `${hour - 12}:${minute} PM` : `${hour}:${minute} AM`
-
-  const day = dateArr.slice(4, 10).join("")
-
-  return [date, time, day]
+  return [date, time]
 }
